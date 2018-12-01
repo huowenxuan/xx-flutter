65f9e1d3538d6ed1ccd826078080d448d722d32a
Wahid <wahid@girtu.com>
2018-12-01 15:20:10 +0800

    feather

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 void main() => runApp(new MyApp());
 
@@ -6,6 +7,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+  	debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
