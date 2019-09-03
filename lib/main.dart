967548c3f1d1ae4b3a9ec896e10cb808b1705ce7
Wahid <wahid@girtu.com>
2019-09-03 17:23:55 +0800

    fork from flutter-ui-nice

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter_ui_nice/page/navigation/navigation1/coordinator.dart';
+import 'package:flutter_ui_nice/page/page_const.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(new MyApp());
+import 'const/string_const.dart';
+import 'const/color_const.dart';
+import 'const/page_str_const.dart';
+import "page/page_const.dart";
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-  	debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return MaterialApp(
+      title: StringConst.APP_NAME,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: BLUE_DEEP,
+        accentColor: YELLOW,
+        fontFamily: "Montserrat",
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+//      home: OriginHomePage(),
+      home: HomePage(),
+      routes: {
+        //PROFILE pages
+        PROFILE_PAGES[0]: (context) => ProfilePageOne(),
+        PROFILE_PAGES[1]: (context) => ProfilePageTwo(),
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+        SIGN_UP_PAGES[0]: (context) => SignPageOne(),
+        SIGN_UP_PAGES[1]: (context) => SignPageTwo(),
+        SIGN_UP_PAGES[2]: (context) => SignPageThree(),
+        SIGN_UP_PAGES[3]: (context) => SignPageFour(),
+        SIGN_UP_PAGES[4]: (context) => SignPageFive(),
+        SIGN_UP_PAGES[5]: (context) => SignPageSix(),
+        SIGN_UP_PAGES[6]: (context) => SignPageSeven(),
+        SIGN_UP_PAGES[7]: (context) => SignPageEight(),
+        SIGN_UP_PAGES[8]: (context) => SignPageNine(),
+        SIGN_UP_PAGES[9]: (context) => SignPageTeen(),
+        SIGN_UP_PAGES[10]: (context) => SignPageEleven(),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+        ///FEED group page
+        FEED_PAGES[0]: (context) => FeedPageOne(),
+        FEED_PAGES[1]: (context) => FeedPageTwo(),
+        FEED_PAGES[2]: (context) => FeedThreePage(),
+        FEED_PAGES[3]: (context) => FeedPageFour(),
+        FEED_PAGES[4]: (context) => FeedFivePage(),
+        FEED_PAGES[9]: (context) => FeedPageTen(),
+        FEED_PAGES[10]: (context) => FeedPageEleven(),
+        FEED_PAGES[11]: (context) => FeedPageTwelve(),
+        FEED_PAGES[12]: (context) => FeedPageThirteen(),
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+        SHOPPING_PAGES[17]: (context) => ShopPageEighteen(),
+        SHOPPING_PAGES[18]: (context) => ShopPageNineteen(),
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        NAVIGATION_PAGES[0]: (context) => NavigationOneCoordinator(),
+        //FIXME there are other pages to jump with 'page_str_const.dart',there should be make by manager
+      },
+      onUnknownRoute: (setting) =>
+          MaterialPageRoute(builder: (context) => EmptyPage()),
     );
   }
 }
