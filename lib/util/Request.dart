91f654914d3cd7d3a933ebb0d3ce388b1a616754
Wahid <wahid@girtu.com>
2019-09-05 14:26:28 +0800

    build macos

--- lib/util/Request.dart
+++ lib/util/Request.dart
@@ -1,34 +1,53 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:dio/dio.dart';
 
 class Request {
+//  static get(url) async {
+//    var httpClient = new HttpClient();
+//    try {
+//      var request = await httpClient.getUrl(Uri.parse(url));
+//      var response = await request.close();
+//      if (response.statusCode == HttpStatus.ok) {
+//        var json = await response.transform(utf8.decoder).join();
+//        var data = jsonDecode(json);
+//        return data;
+//      } else {
+//      }
+//    } catch (exception) {
+//    }
+//  }
+//
+//  static post(url) async {
+//    var httpClient = new HttpClient();
+//    try {
+//      var request = await httpClient.postUrl(Uri.parse(url));
+//      var response = await request.close();
+//      if (response.statusCode == HttpStatus.ok) {
+//        var json = await response.transform(utf8.decoder).join();
+//        var data = jsonDecode(json);
+//        return data;
+//      } else {
+//      }
+//    } catch (exception) {
+//    }
+//  }
+
   static get(url) async {
-    var httpClient = new HttpClient();
     try {
-      var request = await httpClient.getUrl(Uri.parse(url));
-      var response = await request.close();
-      if (response.statusCode == HttpStatus.ok) {
-        var json = await response.transform(utf8.decoder).join();
-        var data = jsonDecode(json);
-        return data;
-      } else {
-      }
-    } catch (exception) {
+      Response response = await Dio().get(url);
+      return response.data;
+    } catch (e) {
+      print(e);
     }
   }
 
-  static post(url) async {
-    var httpClient = new HttpClient();
+  static post(url, data) async {
     try {
-      var request = await httpClient.postUrl(Uri.parse(url));
-      var response = await request.close();
-      if (response.statusCode == HttpStatus.ok) {
-        var json = await response.transform(utf8.decoder).join();
-        var data = jsonDecode(json);
-        return data;
-      } else {
-      }
-    } catch (exception) {
+      Response response = await Dio().post(url, data: data);
+      return response.data;
+    } catch (e) {
+      print(e);
     }
   }
 }
---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/huowenxuan/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/huowenxuan/Desktop/xx-flutter"
+export "FLUTTER_TARGET=/Users/huowenxuan/Desktop/xx-flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Users/huowenxuan/dev/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=5"
